3151d00c4db009fc425c1e6e4cdae1c19477c687
AbhilashBogahawatta <bogahawattaabhilash@gmail.com>
2025-07-22 14:19:22 +0530

    Add the js file

---
+++ Script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let btnValue = e.target.innerHTML;
+
+        if (btnValue === '=') {
+            try {
+                string = eval(string); // Note: use cautiously
+                input.value = string;
+            } catch {
+                input.value = "Error";
+                string = "";
+            }
+        } else if (btnValue === 'AC') {
+            string = "";
+            input.value = string;
+        } else if (btnValue === 'DEL') {
+            string = string.slice(0, -1);
+            input.value = string;
+        } else {
+            string += btnValue;
+            input.value = string;
+        }
+    });
+});
